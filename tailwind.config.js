59205f15e4b965aeb5416e06bf22116c191dbed5
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 01:07:46 +0100

    Favicon + tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        extend: {
+            fontFamily: {
+                'fira-sans': ['Fira Sans', 'sans-serif'],
+                ubuntu: ['Ubuntu', 'sans-serif'],
+                oswald: ['Oswald', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
+                poppins: ['Poppins', 'sans-serif'],
+                roboto: ['Roboto', 'sans-serif']
+            },
+            colors: {
+                text: '#daedf6',
+                background: '#040c10',
+                primary: '#8fc4e3',
+                secondary: '#291f7b',
+                accent: '#6d37cd'
+            }
+        }
     },
     plugins: []
 }
