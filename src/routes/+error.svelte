a8c48c5caf1dd4fbd7c6a58b69896dddb4f255f8
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 01:08:41 +0100

    Error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Icon from '$/components/Icon.svelte'
+    import Navigation from '$/components/navigation.svelte'
+    import { page } from '$app/stores'
+
+    const texts = {
+        404: {
+            title: 'Tak tady jsi špatně',
+            description: 'Tato stránka neexistuje'
+        }
+    }
+
+    const status = $page.status as keyof typeof texts
+</script>
+
+<svelte:head>
+    <title>Error {status} | patrick115.eu</title>
+    <meta name="description" content="Chybová stránka" />
+</svelte:head>
+
+<Navigation />
+
+<div class="flex h-full w-full">
+    <div class="m-auto">
+        <h1 class="font-fira-sans text-4xl font-bold sm:text-5xl"><Icon name="bi-code-slash" /> {texts[status]?.title ?? 'Něco se pokazilo'}</h1>
+        <h2 class="text-2xl font-bold sm:text-3xl">{texts[status]?.description ?? 'Zkus to prosím znovu'}</h2>
+    </div>
+</div>
