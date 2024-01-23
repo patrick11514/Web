e5d6f28d929f42372fba08073fa48b26e912c33f
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 02:11:43 +0100

    Link component

---
+++ src/components/link.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { twMerge } from 'tailwind-merge'
+
+    export let href: string
+    export let external = true
+    let cls = ''
+    export { cls as class }
+</script>
+
+<a class={twMerge('text-primary', cls)} {href} target={external ? '_blank' : '_self'}><slot /></a>
