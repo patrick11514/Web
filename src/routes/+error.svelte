ce7e8d81e4a441a785c9b7937d1968c9e59200a2
Patrik Mintěl <ja@patrick115.eu>
2024-01-23 21:42:49 +0100

    Removed unnecessary div

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -20,9 +20,7 @@
 
 <Navigation />
 
-<div class="flex h-full w-full">
-    <div class="m-auto">
-        <h1 class="font-fira-sans text-4xl font-bold sm:text-5xl"><Icon name="bi-code-slash" /> {texts[status]?.title ?? 'Něco se pokazilo'}</h1>
-        <h2 class="text-2xl font-bold sm:text-3xl">{texts[status]?.description ?? 'Zkus to prosím znovu'}</h2>
-    </div>
+<div class="m-auto">
+    <h1 class="font-fira-sans text-4xl font-bold sm:text-5xl"><Icon name="bi-code-slash" /> {texts[status]?.title ?? 'Něco se pokazilo'}</h1>
+    <h2 class="text-2xl font-bold sm:text-3xl">{texts[status]?.description ?? 'Zkus to prosím znovu'}</h2>
 </div>
