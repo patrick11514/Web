e90862c721f5729127e34346d02ce7432ea17ad3
Patrik Mintěl <ja@patrick115.eu>
2024-02-03 02:44:48 +0100

    About page done

--- src/components/timelinePart.svelte
+++ src/components/timelinePart.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
     export let reverse = false
     export let label: string | undefined = undefined
-    export let src: string
+    export let src:
+        | string
+        | {
+              [selector: string]: string
+              default: string
+          }
     export let alt: string
+
+    //
 </script>
 
-<section class="flex" class:flex-row={!reverse} class:flex-row-reverse={reverse}>
-    <img class="my-2 h-auto w-[45%] self-center px-2" {src} {alt} />
-    <section class="flex w-[10%] flex-col items-center justify-center text-center">
+<section class="flex font-ubuntu lg:mx-auto lg:w-[80%] 3xl:text-xl" class:flex-row={!reverse} class:flex-row-reverse={reverse}>
+    {#if typeof src === 'string'}
+        <img class="my-2 h-auto w-[40%] self-center px-2 xl:w-[45%]" {src} {alt} />
+    {:else}
+        <picture class="my-2 h-auto w-[40%] self-center px-2 xl:w-[45%]">
+            {#each Object.keys(src).filter((src) => src !== 'default') as selector}
+                <source media="({selector})" srcset={src[selector]} />
+            {/each}
+            <img src={src.default} {alt} />
+        </picture>
+    {/if}
+    <section class="flex w-[20%] flex-col items-center justify-center text-center xl:w-[10%]">
         <div class="w-0.5 flex-grow bg-white"></div>
         {#if label !== undefined}
-            <span class="text-wrap font-ubuntu text-lg font-bold">{label}</span>
+            <span class="text-wrap font-ubuntu text-lg font-bold 3xl:text-xl">{label}</span>
         {/if}
         <div class="w-0.5 flex-grow bg-white"></div>
     </section>
-    <span class="my-2 w-[45%] px-2"><slot /></span>
+    <span class="my-2 w-[40%] self-center px-2 xl:w-[45%]" class:text-right={!reverse}><slot /></span>
 </section>
