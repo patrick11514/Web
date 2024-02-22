4b49f2534ec55d2326bb7f026d443f752cb9c712
Patrik Mintěl <ja@patrick115.eu>
2024-02-22 22:52:40 +0100

    Added margin to texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,16 +31,16 @@
     <div class="mx-auto flex w-[90%] flex-col text-justify font-fira-sans text-lg md:my-auto lg:text-xl xl:text-2xl">
         <h2 class="text-center font-ubuntu text-3xl font-bold lg:text-4xl xl:text-5xl">Patrik Mintěl</h2>
         <p class="text-center font-roboto">Věk: {age}</p>
-        <p>
+        <p class="my-1">
             Aktuálně studuju <Link href="https://www.vsb.cz">Vysokou školu Báňskou</Link> v Ostravě. Přesněji studuji obor Informatika na
             <Link href="http://fei.vsb.cz">fakultě elektrotechniky a informatiky</Link>. Ve volném čase se věnuji programováním a hraním her.
         </p>
-        <p>
+        <p class="my-1">
             Nejvíce se věnuji tvorbě webových aplikací a proto nejvíce programuji v
             <Link href="https://www.typescriptlang.org/">TypeScriptu</Link>, kdy se převážně zaměřuji na backend a případně i frontend. U frontendu se převážně zaměřuji na jeho
             funkční část (TypeScript). Aktuální framework, který na tyto účely používám je <Link href="https://kit.svelte.dev/">SvelteKit</Link>. Když už zavítám do designu
             webových stránek, tak využívám <Link href="https://tailwindcss.com/">Tailwind CSS</Link>.
         </p>
-        <p>Mimo tvorbu webových aplikací se věnuji i tvorbě jiných aplikací v NodeJS, jako jsou Discord boti, Twitch boti a podobně.</p>
+        <p class="my-1">Mimo tvorbu webových aplikací se věnuji i tvorbě jiných aplikací v NodeJS, jako jsou Discord boti, Twitch boti a podobně.</p>
     </div>
 </section>
