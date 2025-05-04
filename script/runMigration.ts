48e428d3d2af58ee3002dae84837338b54cda9d2
Patrik Mintěl <ja@patrick115.eu>
2025-05-04 22:36:53 +0200

    Fix migration script

--- script/runMigration.ts
+++ script/runMigration.ts
@@ -3,7 +3,23 @@
 import { Migrator, FileMigrationProvider } from 'kysely';
 import { promises as fs } from 'node:fs';
 import path from 'node:path';
-import { conn } from '../src/types/connection';
+import dotenv from 'dotenv';
+import { Kysely, MysqlDialect } from 'kysely';
+import { createPool } from 'mysql2';
+
+dotenv.config();
+
+const dialect = new MysqlDialect({
+    pool: createPool({
+        host: process.env.DATABASE_IP,
+        port: parseInt(process.env.DATABASE_PORT!),
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME
+    })
+});
+
+const conn = new Kysely({ dialect });
 
 async function runMigrations() {
     const migrator = new Migrator({
