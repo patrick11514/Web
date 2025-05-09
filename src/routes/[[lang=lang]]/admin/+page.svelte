56fc68cd69d0cf3cfd5758ffcf12c246ef57e328
Patrik Mintěl <ja@patrick115.eu>
2025-05-09 17:12:56 +0200

    Updated responsivity

--- src/routes/[[lang=lang]]/admin/+page.svelte
+++ src/routes/[[lang=lang]]/admin/+page.svelte
@@ -25,7 +25,7 @@
     }
 </script>
 
-<div class="flex">
+<div class="flex h-full flex-col items-center justify-center md:flex-row">
     <div class="flex flex-1 flex-col items-center justify-center">
         <h1 class="font-poppins w-max text-3xl font-bold lg:text-4xl"><Icon name="bi-bar-chart" /> {_state.lang.admin.main.stats}</h1>
         <h3>{_state.lang.admin.main.today}: {data.today}</h3>
