a0856fdf17a967417402097161ccc4fe7305267b
Patrik Mintěl <ja@patrick115.eu>
2025-05-11 00:06:04 +0200

    Installed sweet alers

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,24 @@
+import { browser } from '$app/environment';
+import Swal, { type SweetAlertOptions } from 'sweetalert2';
+
 export const sleep = (ms: number) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export const SwalAlert = <$Type = unknown>(data: SweetAlertOptions) => {
+    if (!browser) {
+        return {
+            isConfirmed: false
+        } as const;
+    }
+
+    return Swal.fire<$Type>({
+        toast: true,
+        position: 'top-end',
+        timer: 2000,
+        timerProgressBar: true,
+        showCancelButton: false,
+        showConfirmButton: false,
+        ...data
+    });
 };
