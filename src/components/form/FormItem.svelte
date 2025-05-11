eeb4f8ba05beb19f64f89a7079acbd574fab6bc0
Patrik Mintěl <ja@patrick115.eu>
2025-05-11 13:48:45 +0200

    Added w-full for FormItem

--- src/components/form/FormItem.svelte
+++ src/components/form/FormItem.svelte
@@ -4,7 +4,7 @@
     const { children, for: id, label, error = $bindable(undefined) }: { children: Snippet; for: string; label: string; error?: string } = $props();
 </script>
 
-<div class="flex flex-col">
+<div class="flex w-full flex-col">
     <label for={id} class="font-poppins border-b-text mb-2 w-max border-b-2 text-2xl font-bold lg:text-3xl">{label}</label>
     {@render children()}
     <span class:invisible={error === undefined} class="lg:text-md font-poppins text-base font-bold text-red-500">{error ?? '...'}</span>
