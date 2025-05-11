db233bdf74d538ff5fb2c2549f8630bb8268c722
Patrik Mintěl <ja@patrick115.eu>
2025-05-11 13:48:22 +0200

    Added selecti

---
+++ src/components/form/Select.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import clsx from 'clsx';
+    import type { HTMLSelectAttributes } from 'svelte/elements';
+    import { twMerge } from 'tailwind-merge';
+
+    let { children, class: cls, value = $bindable(), ...props }: HTMLSelectAttributes = $props();
+</script>
+
+<select
+    bind:value
+    class={twMerge('border-secondary focus:border-primary font-roboto rounded-md border-2 px-2 py-1 text-xl font-bold transition-colors duration-200 lg:text-2xl', clsx(cls))}
+    {...props}
+>
+    {@render children?.()}
+</select>
