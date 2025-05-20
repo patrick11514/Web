32680744d6b36e6e4273f9d50fdd62ec54316839
Patrik Mintěl <ja@patrick115.eu>
2025-05-20 02:58:09 +0200

    Update button padding

--- src/components/form/Button.svelte
+++ src/components/form/Button.svelte
@@ -10,7 +10,7 @@
 <button
     {...rest}
     class={twMerge(
-        'border-secondary hover:border-primary font-roboto w-max cursor-pointer rounded-md border-2 px-4 py-2 text-xl font-bold transition-all duration-200 active:translate-y-0.5 disabled:grayscale lg:text-2xl',
+        'border-secondary hover:border-primary font-roboto w-max cursor-pointer rounded-md border-2 px-4 py-1 text-xl font-bold transition-all duration-200 active:translate-y-0.5 disabled:grayscale lg:text-2xl',
         clsx(cls)
     )}
 >
