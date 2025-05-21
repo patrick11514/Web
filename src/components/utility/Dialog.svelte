51b71089b1b201e0c26290a0c033ceaf93d118ed
Patrik Mintěl <ja@patrick115.eu>
2025-05-21 21:26:24 +0200

    Refactor Equipment page

---
+++ src/components/utility/Dialog.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    import ClickOutside from './clickOutside.svelte';
+
+    let { children, opened = $bindable(false), onClose = () => {} }: { children: Snippet; opened: boolean; onClose?: () => void } = $props();
+</script>
+
+{#if opened}
+    <div class="fixed top-0 left-0 z-10 flex h-screen w-screen bg-black/80">
+        <ClickOutside
+            clickoutside={() => {
+                opened = false;
+                onClose();
+            }}
+            class="border-text m-auto flex flex-col rounded-md border-2 bg-black/80 p-4"
+        >
+            {@render children()}
+        </ClickOutside>
+    </div>
+{/if}
--- src/routes/[[lang=lang]]/admin/equipment/+page.svelte
+++ src/routes/[[lang=lang]]/admin/equipment/+page.svelte
@@ -10,31 +10,34 @@
     import { resolveError, replacePlaceholders, resolveTranslation } from '$/lib/lang';
     import { API } from '$/lib/api';
     import { invalidateAll } from '$app/navigation';
-    import ClickOutside from '$/components/utility/clickOutside.svelte';
     import Select from '$/components/form/Select.svelte';
+    import Dialog from '$/components/utility/Dialog.svelte';
 
     const _state = getState();
+    const _lang = $derived(_state.lang.admin.equipment);
 
     const { data }: PageProps = $props();
 
-    const typeAdd = (() => {
-        return async ({ update, result }) => {
-            if (result.type === 'success') {
-                SwalAlert({
-                    title: _state.lang.admin.equipment.types.success,
-                    icon: 'success'
-                });
-                openTypeAdd = false;
-            } else if (result.type === 'failure' && result.data) {
-                SwalAlert({
-                    title: resolveError(result.data.message, _state.lang),
-                    icon: 'error'
-                });
-            }
+    const createSubmitFunction = (successMessage: string, resetFn = () => {}): SubmitFunction => {
+        return (() => {
+            return async ({ update, result }) => {
+                if (result.type === 'success') {
+                    SwalAlert({
+                        title: successMessage,
+                        icon: 'success'
+                    });
+                    resetFn();
+                } else if (result.type === 'failure' && result.data) {
+                    SwalAlert({
+                        title: resolveError(result.data.message, _state.lang),
+                        icon: 'error'
+                    });
+                }
 
-            update();
-        };
-    }) satisfies SubmitFunction;
+                update();
+            };
+        }) satisfies SubmitFunction;
+    };
 
     let typeEditing = $state<null | {
         id: number;
@@ -46,11 +49,11 @@
             timer: 0,
             toast: false,
             position: 'center',
-            title: _state.lang.admin.equipment.types.delete.question,
+            title: _lang.types.delete.question,
             showConfirmButton: true,
-            confirmButtonText: _state.lang.admin.equipment.types.delete.yes,
+            confirmButtonText: _lang.types.delete.yes,
             showCancelButton: true,
-            cancelButtonText: _state.lang.admin.equipment.types.delete.no
+            cancelButtonText: _lang.types.delete.no
         });
 
         if (!result.isConfirmed) return;
@@ -65,51 +68,12 @@
         }
 
         SwalAlert({
-            title: _state.lang.admin.equipment.types.deleteSuccess,
+            title: _lang.types.deleteSuccess,
             icon: 'success'
         });
 
         invalidateAll();
     };
-
-    const typeEdit = (() => {
-        return async ({ update, result }) => {
-            if (result.type === 'success') {
-                SwalAlert({
-                    title: _state.lang.admin.equipment.types.editSuccess,
-                    icon: 'success'
-                });
-
-                typeEditing = null;
-            } else if (result.type === 'failure' && result.data) {
-                SwalAlert({
-                    title: resolveError(result.data.message, _state.lang),
-                    icon: 'error'
-                });
-            }
-
-            update();
-        };
-    }) satisfies SubmitFunction;
-
-    const equipmentAdd = (() => {
-        return async ({ update, result }) => {
-            if (result.type === 'success') {
-                SwalAlert({
-                    title: _state.lang.admin.equipment.equipment.success,
-                    icon: 'success'
-                });
-                openEquipmentAdd = false;
-            } else if (result.type === 'failure' && result.data) {
-                SwalAlert({
-                    title: resolveError(result.data.message, _state.lang),
-                    icon: 'error'
-                });
-            }
-
-            update();
-        };
-    }) satisfies SubmitFunction;
 
     let equipmentEditing = $state<null | {
         id: number;
@@ -118,36 +82,16 @@
         link: string;
     }>(null);
 
-    const equipmentEdit = (() => {
-        return async ({ update, result }) => {
-            if (result.type === 'success') {
-                SwalAlert({
-                    title: _state.lang.admin.equipment.equipment.editSuccess,
-                    icon: 'success'
-                });
-
-                equipmentEditing = null;
-            } else if (result.type === 'failure' && result.data) {
-                SwalAlert({
-                    title: resolveError(result.data.message, _state.lang),
-                    icon: 'error'
-                });
-            }
-
-            update();
-        };
-    }) satisfies SubmitFunction;
-
     const equipmentDelete = async (id: number) => {
         const result = await SwalAlert({
             timer: 0,
             toast: false,
             position: 'center',
-            title: _state.lang.admin.equipment.equipment.delete.question,
+            title: _lang.equipment.delete.question,
             showConfirmButton: true,
-            confirmButtonText: _state.lang.admin.equipment.equipment.delete.yes,
+            confirmButtonText: _lang.equipment.delete.yes,
             showCancelButton: true,
-            cancelButtonText: _state.lang.admin.equipment.equipment.delete.no
+            cancelButtonText: _lang.equipment.delete.no
         });
 
         if (!result.isConfirmed) return;
@@ -162,7 +106,7 @@
         }
 
         SwalAlert({
-            title: _state.lang.admin.equipment.equipment.deleteSuccess,
+            title: _lang.equipment.deleteSuccess,
             icon: 'success'
         });
 
@@ -182,48 +126,6 @@
     <span class="font-poppins m-auto text-2xl font-bold lg:text-3xl"><Icon name="bi-ban-fill" class="text-red-500" /> {text}</span>
 {/snippet}
 
-{#if typeEditing !== null || equipmentEditing !== null}
-    <div class="fixed top-0 left-0 z-10 flex h-screen w-screen bg-black/80">
-        <ClickOutside
-            clickoutside={() => {
-                typeEditing = null;
-                equipmentEditing = null;
-            }}
-            class="border-primary m-auto flex flex-col gap-2 rounded-md border-2 bg-black/80 p-4"
-        >
-            {#if typeEditing !== null}
-                {@render title(replacePlaceholders(_state.lang.admin.equipment.types.edit.title, typeEditing.id.toString()))}
-                <form class="flex flex-col items-center justify-center" method="POST" action="?/typeEdit" use:enhance={typeEdit}>
-                    <input type="hidden" name="id" value={typeEditing.id} />
-                    <FormItem for="key" label={_state.lang.admin.equipment.types.translateKey}>
-                        <Input id="key" name="lang_key" type="text" placeholder={_state.lang.admin.equipment.types.translateKey} value={typeEditing.lang_key} required />
-                    </FormItem>
-                    <Button type="submit">{_state.lang.admin.equipment.types.edit.button}</Button>
-                </form>
-            {:else if equipmentEditing !== null}
-                {@render title(replacePlaceholders(_state.lang.admin.equipment.equipment.edit.title, equipmentEditing.id.toString()))}
-                <form class="flex flex-col items-center justify-center" method="POST" action="?/equipmentEdit" use:enhance={equipmentEdit}>
-                    <input type="hidden" name="id" value={equipmentEditing.id} />
-                    <FormItem for="name" label={_state.lang.admin.equipment.equipment.name}>
-                        <Input id="name" name="name" type="text" placeholder={_state.lang.admin.equipment.equipment.name} value={equipmentEditing.name} required />
-                    </FormItem>
-                    <FormItem for="type" label={_state.lang.admin.equipment.equipment.type}>
-                        <Select id="type" name="type" value={equipmentEditing.type_id} required>
-                            {#each data.types as type (type.id.toString())}
-                                <option value={type.id} selected={equipmentEditing.type_id === type.id}>{resolveTranslation(type.lang_key, _state.lang)}</option>
-                            {/each}
-                        </Select>
-                    </FormItem>
-                    <FormItem for="link" label={_state.lang.admin.equipment.equipment.link}>
-                        <Input id="link" name="link" type="url" placeholder={_state.lang.admin.equipment.equipment.link} value={equipmentEditing.link} required />
-                    </FormItem>
-                    <Button type="submit">{_state.lang.admin.equipment.equipment.edit.button}</Button>
-                </form>
-            {/if}
-        </ClickOutside>
-    </div>
-{/if}
-
 {#snippet _switch(text: string, name: typeof section)}
     <button
         onclick={() => (section = name)}
@@ -235,67 +137,111 @@
     >
 {/snippet}
 
-{#if openTypeAdd}
-    <div class="fixed top-0 left-0 z-10 flex h-screen w-screen bg-black/80">
-        <ClickOutside clickoutside={() => (openTypeAdd = false)} class="border-text m-auto flex flex-col rounded-md border-2 bg-black/80 p-4">
-            {@render title(_state.lang.admin.equipment.types.addTitle)}
-            <form class="border-text flex flex-col items-center justify-center rounded-md border-2 p-2" method="POST" action="?/typeAdd" use:enhance={typeAdd}>
-                <FormItem for="key" label={_state.lang.admin.equipment.types.translateKey}>
-                    <Input id="key" name="lang_key" type="text" required />
-                </FormItem>
-                <Button type="submit">{_state.lang.admin.equipment.types.button}</Button>
-            </form>
-        </ClickOutside>
-    </div>
-{/if}
+<Dialog opened={typeEditing !== null} onClose={() => (typeEditing = null)}>
+    {@render title(replacePlaceholders(_lang.types.edit.title, typeEditing!.id.toString()))}
+    <form
+        class="flex flex-col items-center justify-center"
+        method="POST"
+        action="?/typeEdit"
+        use:enhance={createSubmitFunction(_lang.types.editSuccess, () => (typeEditing = null))}
+    >
+        <input type="hidden" name="id" value={typeEditing!.id} />
+        <FormItem for="key" label={_lang.types.translateKey}>
+            <Input id="key" name="lang_key" type="text" placeholder={_lang.types.translateKey} value={typeEditing!.lang_key} required />
+        </FormItem>
+        <Button type="submit">{_lang.types.edit.button}</Button>
+    </form>
+</Dialog>
 
-{#if openEquipmentAdd}
-    <div class="fixed top-0 left-0 z-10 flex h-screen w-screen bg-black/80">
-        <ClickOutside clickoutside={() => (openEquipmentAdd = false)} class="border-text m-auto flex flex-col rounded-md border-2 bg-black/80 p-4">
-            {@render title(_state.lang.admin.equipment.equipment.addTitle)}
-            <form class="border-text flex flex-col items-center justify-center rounded-md border-2 p-2" method="POST" action="?/equipmentAdd" use:enhance={equipmentAdd}>
-                <FormItem for="name" label={_state.lang.admin.equipment.equipment.name}>
-                    <Input id="name" name="name" type="text" required />
-                </FormItem>
-                <FormItem for="type" label={_state.lang.admin.equipment.equipment.type}>
-                    <Select id="type" name="type" required>
-                        <option value={null} disabled selected></option>
-                        {#each data.types as type (type.id.toString())}
-                            <option value={type.id}>{resolveTranslation(type.lang_key, _state.lang)}</option>
-                        {/each}
-                    </Select>
-                </FormItem>
-                <FormItem for="link" label={_state.lang.admin.equipment.equipment.link}>
-                    <Input id="link" name="link" type="url" required />
-                </FormItem>
-                <Button type="submit">{_state.lang.admin.equipment.equipment.button}</Button>
-            </form>
-        </ClickOutside>
-    </div>
-{/if}
+<Dialog opened={equipmentEditing !== null} onClose={() => (equipmentEditing = null)}>
+    {@render title(replacePlaceholders(_lang.equipment.edit.title, equipmentEditing!.id.toString()))}
+    <form
+        class="flex flex-col items-center justify-center"
+        method="POST"
+        action="?/equipmentEdit"
+        use:enhance={createSubmitFunction(_lang.equipment.editSuccess, () => (equipmentEditing = null))}
+    >
+        <input type="hidden" name="id" value={equipmentEditing!.id} />
+        <FormItem for="name" label={_lang.equipment.name}>
+            <Input id="name" name="name" type="text" placeholder={_lang.equipment.name} value={equipmentEditing!.name} required />
+        </FormItem>
+        <FormItem for="type" label={_lang.equipment.type}>
+            <Select id="type" name="type" value={equipmentEditing!.type_id} required>
+                {#each data.types as type (type.id.toString())}
+                    <option value={type.id}>{resolveTranslation(type.lang_key, _state.lang)}</option>
+                {/each}
+            </Select>
+        </FormItem>
+        <FormItem for="link" label={_lang.equipment.link}>
+            <Input id="link" name="link" type="url" placeholder={_lang.equipment.link} value={equipmentEditing!.link} required />
+        </FormItem>
+        <Button type="submit">{_lang.equipment.edit.button}</Button>
+    </form>
+</Dialog>
+
+<Dialog bind:opened={openTypeAdd}>
+    {@render title(_lang.types.addTitle)}
+    <form
+        class="border-text flex flex-col items-center justify-center rounded-md border-2 p-2"
+        method="POST"
+        action="?/typeAdd"
+        use:enhance={createSubmitFunction(_lang.types.success, () => (openTypeAdd = false))}
+    >
+        <FormItem for="key" label={_lang.types.translateKey}>
+            <Input id="key" name="lang_key" type="text" required />
+        </FormItem>
+        <Button type="submit">{_lang.types.button}</Button>
+    </form>
+</Dialog>
+
+<Dialog bind:opened={openEquipmentAdd}>
+    {@render title(_lang.equipment.addTitle)}
+    <form
+        class="border-text flex flex-col items-center justify-center rounded-md border-2 p-2"
+        method="POST"
+        action="?/equipmentAdd"
+        use:enhance={createSubmitFunction(_lang.equipment.success, () => (openEquipmentAdd = false))}
+    >
+        <FormItem for="name" label={_lang.equipment.name}>
+            <Input id="name" name="name" type="text" required />
+        </FormItem>
+        <FormItem for="type" label={_lang.equipment.type}>
+            <Select id="type" name="type" required>
+                <option value={null} disabled selected></option>
+                {#each data.types as type (type.id.toString())}
+                    <option value={type.id}>{resolveTranslation(type.lang_key, _state.lang)}</option>
+                {/each}
+            </Select>
+        </FormItem>
+        <FormItem for="link" label={_lang.equipment.link}>
+            <Input id="link" name="link" type="url" required />
+        </FormItem>
+        <Button type="submit">{_lang.equipment.button}</Button>
+    </form>
+</Dialog>
 
 <section class="mx-auto flex w-full flex-1 flex-col gap-4 p-4 lg:w-[90%] xl:w-[80%]">
     <div class="text-text-inverse flex w-max gap-1 rounded-md bg-gray-600 p-1 font-bold">
-        {@render _switch(_state.lang.admin.equipment.types.title, 'types')}
-        {@render _switch(_state.lang.admin.equipment.equipment.title, 'equipment')}
+        {@render _switch(_lang.types.title, 'types')}
+        {@render _switch(_lang.equipment.title, 'equipment')}
     </div>
 
     {#if section === 'types'}
         <div class="border-text flex w-full flex-col items-start rounded-md border-2 p-4">
             <div class="flex w-full items-center justify-between">
-                {@render title(_state.lang.admin.equipment.types.title)}
-                <Button onclick={() => (openTypeAdd = true)}>{_state.lang.admin.equipment.types.addTitle}</Button>
+                {@render title(_lang.types.title)}
+                <Button onclick={() => (openTypeAdd = true)}>{_lang.types.addTitle}</Button>
             </div>
             {#if data.types.length === 0}
-                {@render info(_state.lang.admin.equipment.types.empty)}
+                {@render info(_lang.types.empty)}
             {:else}
                 <div class="border-text w-full rounded-md border-2">
                     <table class="[&_tr]:border-b-text w-full table-auto border-collapse text-xl lg:text-2xl [&_tr]:border-b-2">
                         <thead>
                             <tr>
                                 <th>Id</th>
-                                <th>{_state.lang.admin.equipment.types.translateKey}</th>
-                                <th>{_state.lang.admin.equipment.actions}</th>
+                                <th>{_lang.types.translateKey}</th>
+                                <th>{_lang.actions}</th>
                             </tr>
                         </thead>
                         <tbody class="[&_tr:last-child]:border-b-0">
@@ -326,21 +272,21 @@
     {:else}
         <div class="border-text flex w-full flex-col items-start rounded-md border-2 p-4">
             <div class="flex w-full items-center justify-between">
-                {@render title(_state.lang.admin.equipment.equipment.title)}
-                <Button onclick={() => (openEquipmentAdd = true)}>{_state.lang.admin.equipment.equipment.addTitle}</Button>
+                {@render title(_lang.equipment.title)}
+                <Button onclick={() => (openEquipmentAdd = true)}>{_lang.equipment.addTitle}</Button>
             </div>
             {#if data.equipment.length === 0}
-                {@render info(_state.lang.admin.equipment.equipment.empty)}
+                {@render info(_lang.equipment.empty)}
             {:else}
                 <div class="border-text w-full rounded-md border-2">
                     <table class="[&_tr]:border-b-text w-full table-auto border-collapse text-xl lg:text-2xl [&_tr]:border-b-2">
                         <thead>
                             <tr>
                                 <th>Id</th>
-                                <th>{_state.lang.admin.equipment.equipment.name}</th>
-                                <th>{_state.lang.admin.equipment.equipment.type}</th>
-                                <th>{_state.lang.admin.equipment.equipment.link}</th>
-                                <th>{_state.lang.admin.equipment.actions}</th>
+                                <th>{_lang.equipment.name}</th>
+                                <th>{_lang.equipment.type}</th>
+                                <th>{_lang.equipment.link}</th>
+                                <th>{_lang.actions}</th>
                             </tr>
                         </thead>
                         <tbody class="[&_tr:last-child]:border-b-0">
