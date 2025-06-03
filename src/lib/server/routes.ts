feacacb7cd6b6dc8b5140271f324465f75bd0b9a
Patrik Mintěl <ja@patrick115.eu>
2025-06-03 22:34:13 +0200

    Added route for uploads API

--- src/lib/server/routes.ts
+++ src/lib/server/routes.ts
@@ -2,11 +2,13 @@ import { router } from './api';
 import login from './_routes/login';
 import types from './_routes/types';
 import equipment from './_routes/equipment';
+import upload from './_routes/upload';
 
 export const r = router({
     login,
     types,
-    equipment
+    equipment,
+    upload
 });
 
 export type AppRouter = typeof r;
