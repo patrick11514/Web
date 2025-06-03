48480f11ce2afbd80ce441ecdbf2c859a42684f1
Patrik Mintěl <ja@patrick115.eu>
2025-06-03 22:33:49 +0200

    Added class for FormItem

--- src/components/form/FormItem.svelte
+++ src/components/form/FormItem.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+    import clsx from 'clsx';
     import type { Snippet } from 'svelte';
+    import { twMerge } from 'tailwind-merge';
 
     const {
         children,
@@ -7,22 +9,33 @@
         label,
         error = $bindable(undefined),
         variant = 'normal',
-        right
-    }: { children: Snippet; for: string; label: string; error?: string; variant?: 'small' | 'normal'; right?: Snippet } = $props();
+        right,
+        class: cls = ''
+    }: {
+        children: Snippet;
+        for: string;
+        label: string;
+        error?: string;
+        variant?: 'small' | 'normal';
+        right?: Snippet;
+        class?: string;
+    } = $props();
 
     let labelClass = $state('');
 
     switch (variant) {
         case 'normal':
             labelClass = 'text-2xl lg:text-3xl';
+
             break;
+
         case 'small':
             labelClass = 'text-xl lg:text-2xl';
             break;
     }
 </script>
 
-<div class="flex w-full flex-col">
+<div class={twMerge('flex w-full flex-col', clsx(cls))}>
     <div class="flex items-center justify-between">
         <label for={id} class="font-poppins border-b-text mb-2 w-max border-b-2 font-bold {labelClass}">{label}</label>
         {#if right}
