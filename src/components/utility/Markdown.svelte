c7bbb507f03fe996a85c0efd72963595c5ff95c1
Patrik Mintěl <ja@patrick115.eu>
2025-06-03 20:16:53 +0200

    Added markdown component

---
+++ src/components/utility/Markdown.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { marked } from 'marked';
+
+    const { content }: { content: string } = $props();
+</script>
+
+<div class="prose prose-theme prose-lg lg:prose-xl font-medium">
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html marked(content)}
+</div>
