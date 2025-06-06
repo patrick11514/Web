02fd432109ba3aabaeb5bf2d7222cc068530ef62
Patrik Mintěl <ja@patrick115.eu>
2025-06-06 21:11:42 +0200

    Added proper typing for specific inputs: Text/Number

--- src/components/form/Input.svelte
+++ src/components/form/Input.svelte
@@ -3,6 +3,18 @@
     import { twMerge } from 'tailwind-merge';
     import type { FormElement } from '$/types/types';
 
+    type NumberInput = {
+        type: 'number';
+        value?: number | undefined | null;
+        min?: number;
+        max?: number;
+    };
+
+    type TextInput = {
+        type?: 'text' | 'email' | 'password' | 'tel' | 'url';
+        value?: string | undefined | null;
+    };
+
     let {
         value = $bindable(),
         id,
@@ -11,15 +23,16 @@
         type = 'text',
         class: cls,
         error = undefined,
-        required = false
-    }: FormElement<{
-        value?: string | number | undefined | null;
-        id?: string;
-        name?: string;
-        placeholder?: string;
-        type?: 'text' | 'email' | 'password' | 'number' | 'tel' | 'url';
-        required?: boolean;
-    }> = $props();
+        required = false,
+        ...props
+    }: FormElement<
+        (NumberInput | TextInput) & {
+            id?: string;
+            name?: string;
+            placeholder?: string;
+            required?: boolean;
+        }
+    > = $props();
 </script>
 
 <input
@@ -28,6 +41,7 @@
     {name}
     {placeholder}
     {required}
+    {...props}
     bind:value
     class={twMerge(
         'border-secondary focus:border-primary font-roboto placeholder:font-roboto placeholder:text-text rounded-md border-2 px-2 py-1 text-xl font-bold transition-colors duration-200 outline-none placeholder:font-bold lg:text-2xl',
