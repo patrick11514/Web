7afa4adaca58292d5cc81ed263031f464bdd77ab
Patrik Mintěl <ja@patrick115.eu>
2025-06-07 15:07:19 +0200

    Updated logging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,15 +6,21 @@ export const handle = (async ({ event, resolve }) => {
     const path = event.url.pathname;
 
     const disallowedPaths = ['/api'];
+    const UA = event.request.headers.get('user-agent') || '';
+    const isBot = /bot|crawl|spider|slurp/i.test(UA);
 
-    if (response.status === 200 && !event.locals.is404 && !disallowedPaths.some((p) => path.startsWith(p))) {
+    if (!isBot && response.status === 200 && !event.locals.is404 && !disallowedPaths.some((p) => path.startsWith(p))) {
         conn.insertInto('visitors')
             .values({
                 ip: event.getClientAddress(),
                 page: path,
                 user_agent: event.request.headers.get('user-agent') || ''
             })
-            .execute();
+            .execute()
+        .catch((err) => {
+                //eslint-disable-next-line no-console
+            console.error('Error logging visitor:', err);
+            });
     }
 
     return response;
