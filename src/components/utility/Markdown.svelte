3600f287f77454ebfec1638fb7c096564e2e1ce9
Patrik Mintěl <ja@patrick115.eu>
2025-06-16 23:06:53 +0200

    Dots for element list

--- src/components/utility/Markdown.svelte
+++ src/components/utility/Markdown.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
+    import clsx from 'clsx';
+    import type { ClassValue } from 'clsx';
     import { marked } from 'marked';
+    import { twMerge } from 'tailwind-merge';
 
-    const { content }: { content: string } = $props();
+    const { content, class: cls = '' }: { content: string; class?: ClassValue } = $props();
 </script>
 
-<div class="prose prose-theme prose-lg lg:prose-xl font-medium">
+<div class={twMerge('prose prose-theme prose-lg lg:prose-xl font-medium', clsx(cls))}>
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html marked(content)}
 </div>
